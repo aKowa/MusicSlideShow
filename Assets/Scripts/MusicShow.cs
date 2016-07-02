1a0d065c3e7fd0494f29d94f01563457eddd1f77
Andre Kowalewski <a.kowa@gmx.de>
2016-07-02 18:45:48 +0200

    [FINISHED] image and music logic

---
+++ Assets/Scripts/MusicShow.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Linq;
+
+public class MusicShow
+{
+	public readonly int year;
+	private readonly AudioClip[] musicClips;
+	private int clipID;
+	private readonly Sprite[] sprites;
+	private int spriteID;
+
+
+	public MusicShow( AudioClip[] newClips, Sprite[] newSprites, int newYear )
+	{
+		musicClips = newClips;
+		sprites = newSprites;
+		year = newYear;
+		clipID = -1;
+		spriteID = -1;
+	}
+
+
+	public float Duration
+	{
+		get
+		{
+			return musicClips.Sum(m => m.length);
+		}
+	}
+
+
+	public float DurationPerSprite()
+	{
+		return Duration / sprites.Length;
+	}
+
+
+	public AudioClip GetNextClip()
+	{
+		++clipID;
+		return clipID < musicClips.Length ? musicClips[clipID] : null;
+	}
+
+
+	public Sprite GetNextSprite()
+	{
+		++spriteID;
+		return spriteID < sprites.Length ? sprites[spriteID] : null;
+	}
+}
---
+++ Assets/Scripts/MusicShowManager.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using UnityEngine.UI;
+
+public class MusicShowManager : MonoBehaviour
+{
+	public Image image;
+	public AudioSource audioSource;
+	public int[] years;
+	public float minDuration = 5f;
+	public float maxDuration = 10f;
+	private int yearID = -1;
+	private MusicShow currentMusicShow;
+
+
+	private void Start ()
+	{
+		yearID = -1;
+		SetNextMusicShow();
+	}
+
+
+	private IEnumerator Show()
+	{
+		var sprite = currentMusicShow.GetNextSprite();
+		if ( sprite == null ) yield break;
+		image.sprite = sprite;
+		yield return new WaitForSeconds( currentMusicShow.DurationPerSprite() );
+		StartCoroutine( Show() );
+	}
+
+
+	private IEnumerator Play()
+	{
+		var clip = currentMusicShow.GetNextClip ();
+
+		if (clip == null)
+		{
+			SetNextMusicShow();
+			yield break;
+		}
+
+		audioSource.clip = clip;
+		audioSource.Play();
+		yield return new WaitForSeconds ( audioSource.clip.length );
+		StartCoroutine( Play() );
+	}
+
+
+	public void SetNextMusicShow()
+	{
+		++yearID;
+		if (yearID < years.Length)
+		{
+			var sprites = (Sprite[])Resources.LoadAll<Sprite> ( years[yearID].ToString () );
+			var clips = (AudioClip[])Resources.LoadAll<AudioClip> ( years[yearID].ToString () );
+			currentMusicShow = new MusicShow ( clips, sprites, years[yearID] );
+
+			StopAllCoroutines ();
+			StartCoroutine ( Play () );
+			StartCoroutine ( Show () );
+
+			Debug.Log ( "Year: " + years[yearID] + "  Duration: " + currentMusicShow.Duration + "s" + "  Duration per sprite: " + currentMusicShow.DurationPerSprite() + "s" );
+			if (currentMusicShow.DurationPerSprite() < minDuration)
+			{
+				Debug.LogError("Duration per sprite too low. Use less sprites or longer music!");
+			}
+			else if ( currentMusicShow.DurationPerSprite() > maxDuration )
+			{
+				Debug.LogError ( "Duration per sprite too high. Use more sprites or shorten music!" );
+			}
+		}
+		else
+		{
+			OnEnd();
+		}
+	}
+
+	private void OnEnd()
+	{
+		Debug.Log("All Done");
+	}
+}
